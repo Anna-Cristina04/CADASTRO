5892108c4ea6201ea327141217bcdaf00eac0ee3
ANNA CRISTINA KOERICH <anna_koerich@estudante.sc.senai.br>
2022-10-27 21:51:16 -0300

    primeiro

---
+++ src/app/atividade/cadastrar/cadastrar.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CadastrarComponent } from './cadastrar.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [
+    CadastrarComponent
+  ]
+})
+export class CadastrarModule {
+
+ }
---
+++ src/app/atividade/consultar/consultar.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ConsultarComponent } from './consultar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ConsultarComponent
+  ]
+})
+export class ConsultarModule { 
+  
+}
